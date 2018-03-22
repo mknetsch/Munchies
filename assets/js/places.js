09bbc93eee274a6f1350afab7140830a47b85517
Lappy Mc Lap Face <ephillipsiii>
2018-03-21 20:22:49 -0600

    more attempts at getting a return from the api

--- assets/js/places.js
+++ assets/js/places.js
@@ -3,11 +3,11 @@
 $(document).ready(function() {
 
     $("#submit").click(function() {
-        var queryURL = "https://maps.googleapis.com/place/nearbysearch/dispensary/api/js?=";
+        var queryURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=5000&type=Dispensaries&key=";
         var apiKey = "AIzaSyBa5pLEhR0kzF2FR2f7hRIv7lgtR8U3n84"
         //had to snag a plugin in Chrome to get the API key to return "Allow-Control-Allow-Origin" error
-        var userAddress = "&street-address=" + $("#userStreet").val().trim() + $("#userCity").val().trim() + ", " + $("#userState").val().trim();
-        queryURL = encodeURI(queryURL + userAddress + apiKey)
+        var location = "&location=" + $("#userStreet").val().trim() + $("#userCity").val().trim() + ", " + $("#userState").val().trim();
+        queryURL = encodeURI(queryURL + location + apiKey)
         console.log(queryURL);
 
     //     $.ajax({
