1428b1d02e40a5746d5fc191219ba38a6f72a885
Lappy Mc Lap Face <ephillipsiii>
2018-03-21 19:55:58 -0600

    troubleshootig/api key debugging'
    ;lasfjas
    git commit -m

--- assets/js/places.js
+++ assets/js/places.js
@@ -3,10 +3,18 @@
 $(document).ready(function() {
 
     $("#submit").click(function() {
-        var queryURL = "https://maps.googleapis.com/maps/api/js?=";
+        var queryURL = "https://maps.googleapis.com/place/nearbysearch/dispensary/api/js?=";
         var apiKey = "AIzaSyBa5pLEhR0kzF2FR2f7hRIv7lgtR8U3n84"
-        var userAddress = "&street-address=" + $("#userStreet").val() + $("#userCity").val() + ", " + $("#userState").val();
+        //had to snag a plugin in Chrome to get the API key to return "Allow-Control-Allow-Origin" error
+        var userAddress = "&street-address=" + $("#userStreet").val().trim() + $("#userCity").val().trim() + ", " + $("#userState").val().trim();
         queryURL = encodeURI(queryURL + userAddress + apiKey)
         console.log(queryURL);
-    })
+
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: "GET"
+    //     }).then(function (response) {
+    //         console.log(response);
+    // });
+})
 });
