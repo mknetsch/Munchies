1ec3a0c5d4d15d94798638a23f922d6546ff24bc
Lappy Mc Lap Face <ephillipsiii>
2018-03-21 19:05:42 -0600

    initial google places functionality

--- assets/js/places.js
+++ assets/js/places.js
@@ -1,1 +1,12 @@
 // google places api key = AIzaSyBa5pLEhR0kzF2FR2f7hRIv7lgtR8U3n84
+
+$(document).ready(function() {
+
+    $("#submit").click(function() {
+        var queryURL = "https://maps.googleapis.com/maps/api/js?=";
+        var apiKey = "AIzaSyBa5pLEhR0kzF2FR2f7hRIv7lgtR8U3n84"
+        var userAddress = "&street-address=" + $("#userStreet").val() + $("#userCity").val() + ", " + $("#userState").val();
+        queryURL = encodeURI(queryURL + userAddress + apiKey)
+        console.log(queryURL);
+    })
+});
